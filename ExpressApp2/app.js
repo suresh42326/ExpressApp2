6d895d6c2971b42b3215e8dc4c9f6bfeb1223e78
Suresh Karri <suresh42326@gmail.com>
2023-02-25 18:27:14 +0530

    feature/1 commit

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -51,6 +51,7 @@ if (app.get('env') === 'development') {
 // production error handler
 //comment from Github web
 // no stacktraces leaked to user
+//comment in feature/1 branch
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
